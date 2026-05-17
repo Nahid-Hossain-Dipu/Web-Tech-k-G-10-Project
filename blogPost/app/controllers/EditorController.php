24fecd64f617b01860ee0fd5b28bf4589059d74f
Kaniz Fatema Rimi <kanizrimi18@gmail.com>
2026-05-17 17:27:51 +0600

    Added View Editor Dashboard

---
+++ blogPost/app/controllers/EditorController.php
@@ -0,0 +1,122 @@
+<?php
+
+class EditorController{
+
+
+    function dashboard(){
+
+        $article =
+        new Article();
+
+        $data = [
+
+        "submitted" =>
+
+        mysqli_fetch_assoc(
+
+        $article
+        ->getSubmittedCount()
+
+        ),
+
+        "approved" =>
+
+        mysqli_fetch_assoc(
+
+        $article
+        ->getApprovedCount()
+
+        ),
+
+        "scheduled" =>
+
+        mysqli_fetch_assoc(
+
+        $article
+        ->getScheduledCount()
+
+        ),
+
+        "published" =>
+
+        mysqli_fetch_assoc(
+
+        $article
+        ->getPublishedCount()
+
+        )
+
+        ];
+
+        return $data;
+
+    }
+
+
+
+    function queue(){
+
+        $article =
+        new Article();
+
+        return
+        $article
+        ->getQueueArticles();
+
+    }
+
+
+
+    function review($id){
+
+        $article =
+        new Article();
+
+        return
+        $article
+        ->getArticleById(
+            $id
+        );
+
+    }
+
+
+
+    function updateStatus(
+        $id,
+        $status,
+        $feedback
+    ){
+
+        $article =
+        new Article();
+
+        return
+        $article
+        ->updateArticleStatus(
+
+            $id,
+
+            $status,
+
+            $feedback
+
+        );
+
+    }
+    function tags($id){
+
+    $article=
+    new Article();
+
+    return
+    $article
+    ->getTags(
+        $id
+    );
+
+}
+
+}
+
+?>
