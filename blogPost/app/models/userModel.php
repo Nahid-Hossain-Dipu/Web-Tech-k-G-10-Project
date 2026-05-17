b2d4e42414eb904a8d0832f2a025c8b28252c522
Mahmudul Hasan <mdmhamudulemon@gmail.com>
2026-05-17 17:52:22 +0600

    Register (#5)

--- blogPost/app/models/userModel.php
+++ blogPost/app/models/userModel.php
@@ -1,3 +1,23 @@
+<!--register-->
+<?php
+function createUser($conn, $name, $username, $email, $password){
+    $sql = "INSERT INTO users(name, username, email, password_hash)
+            VALUES(?,?,?,?)";
+
+    $stmt = mysqli_prepare($conn, $sql);
+
+    mysqli_stmt_bind_param(
+        $stmt,
+        "ssss",
+        $name,
+        $username,
+        $email,
+        $password
+    );
+    return mysqli_stmt_execute($stmt);
+}
+?>
+<!--login-->
 <?php
 
 function getUserByEmail($conn, $email) {
